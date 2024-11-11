139497ca93c42c9871e52612de2a9de3a2757237
nurul wahidah <nurulwahidah430@gmail.com>
2024-11-11 23:03:17 +0700

    add commit

---
+++ app/src/main/java/nurul/polbeng/ac/id/sqliteexample/StudentModel.kt
@@ -0,0 +1,10 @@
+package nurul.polbeng.ac.id.sqliteexample
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+@Parcelize
+data class StudentModel (
+    val nim: String,
+    val name: String,
+    val age: String
+): Parcelable
